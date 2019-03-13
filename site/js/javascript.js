d390379846413e7fb4d3ee11fc0b3dfa81a6ad28
Roel Nieskens <roel@pixelambacht.nl>
2019-03-13 16:19:39 +0100

    Fix JS random/interval, add closeToast

--- site/js/javascript.js
+++ site/js/javascript.js
@@ -1,18 +1,23 @@
 // Start periodic addition of toast
 if(useToast === "interval") {
   setInterval(function() {
-    renderToast(Math.floor(Math.random() * Math.floor(4)));
+    renderToast(Math.floor(Math.random() * Math.floor(3)));
   }, (toastInterval * 1000));
 }
 
-// Handle click on button
+// Handle click on "Add toast" button
 function addToast() {
   if(isNaN(useToast)) {
-    msg = Math.floor(Math.random() * Math.floor(4));
+    msg = Math.floor(Math.random() * Math.floor(3));
   } else {
     msg = useToast;
   }
   renderToast(msg);
+}
+
+// Handle click on close button of toast
+function closeToast(e) {
+  e.parentNode.remove();
 }
 
 // Take a template and add it to the toasts container
