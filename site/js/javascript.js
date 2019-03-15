cd655b487b63ac2ccd790d11459b6f954416ac1a
Roel Nieskens <roel@pixelambacht.nl>
2019-03-15 16:41:34 +0100

    Add basics for an app layout

--- site/js/javascript.js
+++ site/js/javascript.js
@@ -1,5 +1,5 @@
 // Start periodic addition of toast
-if(useToast === "interval") {
+if(useToast === 'interval') {
   setInterval(function() {
     renderToast(Math.floor(Math.random() * Math.floor(3)));
   }, (toastInterval * 1000));
@@ -20,6 +20,15 @@ function closeToast(e) {
   e.parentNode.remove();
 }
 
+function clearToasts() {
+  var container = document.getElementById('toasts');
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+
+  updateCount();
+}
+
 // Take a template and add it to the toasts container
 function renderToast(msg) {
   var messages = document.getElementsByTagName('template'),
@@ -27,4 +36,21 @@ function renderToast(msg) {
     newToast = messages[msg].content.cloneNode(true);
 
   target.appendChild(newToast);
+
+  updateCount();
+}
+
+// Show icon when we have one or more toasts visible
+function updateCount() {
+  var visibleToasts = document.getElementById('toasts').children.length;
+  if(visibleToasts === 0) {
+    document.querySelector('.counter').remove();
+  } else {
+    if(!document.querySelector('.counter')) {
+      var counterEl = document.createElement('div');
+      counterEl.className = 'counter';
+      counterEl.innerHTML = '🍞'
+      document.querySelector('header').appendChild(counterEl);
+    }
+  }
 }
